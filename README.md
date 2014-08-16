940e2f7b8a87887c082423bb33ed4a4533f8acfd
Dhanesh Purohit <dhanesh.purohit@gmail.com>
2014-08-16 21:31:42 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+A short script that sorts the files into extension named folders.
+
+Mainly created to sort the mess in my downloads folder, but should work on any folder.
+
+Have tried to use only standard library to not have any dependency issues.
---
+++ app.js
@@ -0,0 +1,27 @@
+var fs = require('fs'),
+	path = require('path');
+
+var argv = process.argv;
+var dir = argv[2];
+
+if(fs.existsSync(dir)){
+	
+	var ls = fs.readdirSync(dir);
+	
+	for (var i = 0; i < ls.length; i++){
+		var file = ls[i];
+		var filepath = path.join(dir, file);
+		var folder = 
+		path.join(dir, path.extname(file).toLowerCase().replace('.',''));
+		var newfilepath = path.join(folder, file);
+		if(fs.existsSync(folder)){
+			fs.renameSync(filepath, newfilepath);
+		}else{
+			fs.mkdirSync(folder);
+			console.log('Moving '+filepath+' to '+newfilepath);
+			fs.renameSync(filepath, newfilepath);
+		}
+	}
+}else{
+	console.log('Invalid directory');
+}
